53d2d6763b38ce7ddd3cadfb9bd3f15539b60140
iDebonair <ofemiowolabi@gmail.com>
2023-09-18 19:16:08 -0600

    Added game logic class to run the core of the game

--- player.rb
+++ player.rb
@@ -1,15 +1,18 @@
 class Player
     attr_accessor :name, :lives
 
+    # Initialize player with a name and 3 lives
     def initialize(name)
         @name = name
         @lives = 3
     end
 
+    #if players gets the question wrong, decrease lives by 1
     def lose_life
         @lives -= 1
     end
 
+    # Checks if player has lives left
     def game_over
         @lives == 0
     end
